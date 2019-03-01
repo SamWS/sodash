66c8577019afe1648d0a97335248901f910e35c9
Sam West-Sooby <s.west.sooby@gmail.com>
2019-03-01 23:38:23 +1100

    add intersect and intersect tests

--- array/intersection/index.js
+++ array/intersection/index.js
@@ -4,6 +4,16 @@
  * the first array.
  */
 
-module.exports = function intersection() {
+module.exports = function intersection(arr1, arr2) {
+  const intersectedArray = [];
 
+  for (var i = 0; i < arr1.length; i++) {
+    for (var j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j] && !intersectedArray.includes(arr1[i])) {
+        intersectedArray.push(arr1[i]);
+      }
+    }
+  }
+
+  return intersectedArray;
 }
---
+++ array/intersection/index.test.js
@@ -0,0 +1,23 @@
+const intersection = require('.');
+
+describe('intersection', () => {
+
+  it('should return correct array', () => {
+    expect(intersection([2, 1], [2, 3])).toEqual([2]);
+  });
+
+  it('should return only unique values', () => {
+    const arr1 = [2, 1, 2, 5, 4];
+    const arr2 = [2, 3, 2, 5, 0];
+
+    expect(intersection(arr1, arr2)).toEqual([2, 5]);
+  });
+
+  it('should return an empty array if no values intersect', () => {
+    const arr1 = [1, 2, 3];
+    const arr2 = [4, 5, 6];
+    
+    expect(intersection(arr1, arr2)).toEqual([]);
+  })
+
+});
