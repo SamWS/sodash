5d45f3826f6830fb5b6ec2c553027bc82a33595f
Sam West-Sooby <s.west.sooby@gmail.com>
2019-03-01 23:16:32 +1100

    refactored names

--- array/concat/index.js
+++ array/concat/index.js
@@ -1,6 +1,6 @@
 /* Creates a new array concatenating array with any additional arrays and/or values. */
 
-module.exports = function conc(arr) {
+module.exports = function concat(arr) {
   const copyArgs = [...arguments];
   let first = copyArgs[0];
   const allNeededArgs = copyArgs.slice(1);
---
+++ array/intersection/index.js
@@ -0,0 +1,9 @@
+/* Creates an array of unique values that are included in 
+ * all given arrays using SameValueZero for equality comparisons. 
+ * The order and references of result values are determined by 
+ * the first array.
+ */
+
+module.exports = function intersection() {
+
+}
